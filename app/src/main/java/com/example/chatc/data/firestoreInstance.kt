388199fea7ff6cb7b1a8758271aecadf97781626
Achintya Sharma <sharmaachintya08@gmail.com>
2023-01-29 16:18:58 +0530

    created a firestore instance class to store data into firestore in Firestore package with name firestoreInstance.kt

---
+++ app/src/main/java/com/example/chatc/data/firestoreInstance.kt
@@ -0,0 +1,31 @@
+package com.example.chatc.data
+
+import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class firestoreInstance(
+    private val image : String,
+    private val name: String,
+    private val email : String,
+    private val password : String,
+    private val confirmPassword : String){
+    fun addInstance(){
+        val db = Firebase.firestore
+        val user = hashMapOf(
+            "image" to image,
+            "name" to name,
+            "email" to email,
+            "password" to password,
+            "confirmPassword" to confirmPassword
+        )
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.i("addedDetails","${documentReference.id}")
+            }
+            .addOnFailureListener{ error ->
+                Log.d("error","${error}")
+            }
+    }
+}
