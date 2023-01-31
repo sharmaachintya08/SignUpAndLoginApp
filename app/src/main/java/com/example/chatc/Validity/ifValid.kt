b4fa7704382af273fee8abf62904fba62bee75c8
Achintya Sharma <sharmaachintya08@gmail.com>
2023-01-31 14:28:57 +0530

    bugs solved in ifValid.kt signupvalid class

--- app/src/main/java/com/example/chatc/Validity/ifValid.kt
+++ app/src/main/java/com/example/chatc/Validity/ifValid.kt
@@ -1,40 +1,42 @@
 package com.example.chatc.Validity
 
 import android.content.Context
+import android.util.Log
+import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.storage.StorageReference
 
-//bugs in if and else
 class ifValid(
     private val imageRef : StorageReference ,
-    private val name : String,
-    private val email : String,
-    private val password : String,
-    private val confirmPassword : String
+    private val name : EditText,
+    private val email : EditText,
+    private val password : EditText,
+    private val confirmPassword : EditText
 ){
     fun signUpValid(con : Context) : Int {
-        if(name.trim() == ""){
+        Log.d("valid","${password.text.trim()} , ${confirmPassword.text.trim()}")
+        if(name.text.toString().trim().isEmpty()){
             Toast.makeText(con,"please fill the name field",Toast.LENGTH_SHORT)
                 .show()
             return 1
-        }else if(email.trim().equals("")){
+        }else if(email.text.toString().trim().isEmpty()){
             Toast.makeText(con,"please fill the email field",Toast.LENGTH_SHORT)
                 .show()
             return 1
-        }else if(!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+        }else if(!android.util.Patterns.EMAIL_ADDRESS.matcher(email.text.toString()).matches()){
             Toast.makeText(con,"please fill the correct email",Toast.LENGTH_SHORT)
                 .show()
             return 1
-        }else if(password.trim().equals("")){
+        }else if(password.text.toString().trim().isEmpty()){
             Toast.makeText(con,"please fill the password field",Toast.LENGTH_SHORT)
                 .show()
             return 1
-        }else if(confirmPassword.trim().equals("")){
+        }else if(confirmPassword.text.toString().trim().isEmpty()){
             Toast.makeText(con,"please fill the confirm password field",Toast.LENGTH_SHORT)
                 .show()
             return 1
-        }else if(!password.trim().equals(confirmPassword)){
-            Toast.makeText(con,"password and confirm password field does not match",Toast.LENGTH_SHORT)
+        }else if(!confirmPassword.text.toString().equals(password.text.toString())){
+            Toast.makeText(con,"password and confirm password field do not match",Toast.LENGTH_SHORT)
                 .show()
             return 1
         }else{
