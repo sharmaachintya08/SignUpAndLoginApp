2b8e71bef1e46b7cbf21eeb5cd43a7f360c56cf3
Achintya Sharma <sharmaachintya08@gmail.com>
2023-02-01 19:08:00 +0530

    simplified ifValid.kt and added returnVal fun

--- app/src/main/java/com/example/chatc/Validity/ifValid.kt
+++ app/src/main/java/com/example/chatc/Validity/ifValid.kt
@@ -13,95 +13,83 @@ class ifValid(
     private val email : EditText?,
     private val password : EditText?,
     private val confirmPassword : EditText?
-){
-    fun signUpValid(con : Context) : Boolean {
-        if(isPresent()){
-            Toast.makeText(con,"account is present, please Login",Toast.LENGTH_SHORT)
+) {
+    private val fInstance = firestoreInstance(storageRef, name?.text.toString(), email?.text.toString(), password?.text.toString(), confirmPassword?.text.toString())
+    fun signUpValid(con: Context): Boolean {
+        if (returnVal()) {
+            Toast.makeText(con, "account is present, please Login", Toast.LENGTH_SHORT)
                 .show()
             return false
-        }else if(name?.text.toString().trim().isEmpty()){
-            Toast.makeText(con,"please fill the name field",Toast.LENGTH_SHORT)
+        } else if (name?.text.toString().trim().isEmpty()) {
+            Toast.makeText(con, "please fill the name field", Toast.LENGTH_SHORT)
                 .show()
             return false
-        }else if(email?.text.toString().trim().isEmpty()){
-            Toast.makeText(con,"please fill the email field",Toast.LENGTH_SHORT)
+        } else if (email?.text.toString().trim().isEmpty()) {
+            Toast.makeText(con, "please fill the email field", Toast.LENGTH_SHORT)
                 .show()
             return false
-        }else if(!android.util.Patterns.EMAIL_ADDRESS.matcher(email?.text.toString()).matches()){
-            Toast.makeText(con,"please fill the correct email",Toast.LENGTH_SHORT)
+        } else if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email?.text.toString()).matches()) {
+            Toast.makeText(con, "please fill the correct email", Toast.LENGTH_SHORT)
                 .show()
             return false
-        }else if(password?.text.toString().trim().isEmpty()){
-            Toast.makeText(con,"please fill the password field",Toast.LENGTH_SHORT)
+        } else if (password?.text.toString().trim().isEmpty()) {
+            Toast.makeText(con, "please fill the password field", Toast.LENGTH_SHORT)
                 .show()
             return false
-        }else if(confirmPassword?.text.toString().trim().isEmpty()){
-            Toast.makeText(con,"please fill the confirm password field",Toast.LENGTH_SHORT)
+        } else if (confirmPassword?.text.toString().trim().isEmpty()) {
+            Toast.makeText(con, "please fill the confirm password field", Toast.LENGTH_SHORT)
                 .show()
             return false
-        }else if(!confirmPassword?.text.toString().equals(password?.text.toString())){
-            Toast.makeText(con,"password and confirm password field do not match",Toast.LENGTH_SHORT)
+        } else if (!confirmPassword?.text.toString().equals(password?.text.toString())) {
+            Toast.makeText(
+                con,
+                "password and confirm password field do not match",
+                Toast.LENGTH_SHORT
+            )
                 .show()
             return false
-        }else{
-            Toast.makeText(con,"successfully signed up",Toast.LENGTH_SHORT)
+        } else {
+            Toast.makeText(con, "successfully signed up", Toast.LENGTH_SHORT)
                 .show()
             sendDataToFireStore()
             return true
         }
     }
-    fun signInValid(con : Context) : Boolean{
-        if(email?.text.toString().isEmpty()){
-            Toast.makeText(con,"please fill the email field",Toast.LENGTH_SHORT)
+
+    fun signInValid(con: Context): Boolean {
+        if (email?.text.toString().isEmpty()) {
+            Toast.makeText(con, "please fill the email field", Toast.LENGTH_SHORT)
                 .show()
             return false
-        }else if(password?.text.toString().isEmpty()){
-            Toast.makeText(con,"please fill the password field",Toast.LENGTH_SHORT)
+        } else if (password?.text.toString().isEmpty()) {
+            Toast.makeText(con, "please fill the password field", Toast.LENGTH_SHORT)
                 .show()
             return false
-        }else{
+        } else {
             return true
         }
     }
-    fun guestValid(con : Context) : Boolean{
-        if(name?.text.toString().isEmpty()){
-            Toast.makeText(con,"please fill the name field",Toast.LENGTH_SHORT)
+
+    fun guestValid(con: Context): Boolean {
+        if (name?.text.toString().isEmpty()) {
+            Toast.makeText(con, "please fill the name field", Toast.LENGTH_SHORT)
                 .show()
             return false
-        }else{
-            Toast.makeText(con,"welcome as guest",Toast.LENGTH_SHORT)
+        } else {
+            Toast.makeText(con, "welcome as guest", Toast.LENGTH_SHORT)
                 .show()
             return true
         }
     }
-    fun sendDataToFireStore(){
-        firestoreInstance(
-            storageRef,
-            name?.text.toString(),
-            email?.text.toString(),
-            password?.text.toString(),
-            confirmPassword?.text.toString()
-        ).addInstance()
+
+    fun sendDataToFireStore() {
+        fInstance.addInstance()
     }
-    fun getDataFromFireStore() : Boolean{
-        val returnVal = firestoreInstance(
-            storageRef,
-            name?.text.toString(),
-            email?.text.toString(),
-            password?.text.toString(),
-            confirmPassword?.text.toString()
-        ).getData()
-        return returnVal
+
+    fun getDataFromFireStore(){
+        fInstance.getData()
     }
-    fun isPresent() : Boolean{
-        val returnVal = firestoreInstance(
-            storageRef,
-            name?.text.toString(),
-            email?.text.toString(),
-            password?.text.toString(),
-            confirmPassword?.text.toString()
-        ).getData()
-        Log.d("isPresent"," isPresent :- ${returnVal}")
-        return returnVal
+    fun returnVal() : Boolean{
+        return fInstance.returnVal()
     }
 }
