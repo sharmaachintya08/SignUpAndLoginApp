da4a52c0013504641752a47a7811fbcab4f62c72
Achintya Sharma <sharmaachintya08@gmail.com>
2023-02-03 11:05:04 +0530

    created a new activity messageBox.kt

---
+++ app/src/main/java/com/example/chatc/Message/messageBox.kt
@@ -0,0 +1,12 @@
+package com.example.chatc.Message
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.chatc.R
+
+class messageBox : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_message_box)
+    }
+}
