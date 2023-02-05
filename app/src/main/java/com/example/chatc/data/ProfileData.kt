34a0848e52c6934f1c1e6ebd6e5eedd1b53d6cfa
Achintya Sharma <sharmaachintya08@gmail.com>
2023-02-05 15:27:50 +0530

    created a common data class from where the recyclerview will get the data

--- app/src/main/java/com/example/chatc/data/ProfileData.kt
+++ app/src/main/java/com/example/chatc/data/ProfileData.kt
@@ -5,7 +5,5 @@ import com.google.firebase.storage.StorageReference
 data class ProfileData(
     val imageRef : StorageReference?,
     val name : String,
-    val email : String,
-    val password : String,
-    val confirmPassword : String
+    val message : String
 )
