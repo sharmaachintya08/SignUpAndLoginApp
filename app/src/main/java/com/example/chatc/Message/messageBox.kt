4105aab7bf09908eeb0e520640f99167cd01d3da
Achintya Sharma <sharmaachintya08@gmail.com>
2023-02-05 16:27:10 +0530

    created function recyclerviewreference

--- app/src/main/java/com/example/chatc/Message/messageBox.kt
+++ app/src/main/java/com/example/chatc/Message/messageBox.kt
@@ -2,11 +2,22 @@ package com.example.chatc.Message
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.chatc.R
+import com.example.chatc.data.dummyData
 
-class messageBox : AppCompatActivity() {
+open class messageBox : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_message_box)
+        recyclerViewReference()
+    }
+    fun recyclerViewReference(){
+        val recyclerView = findViewById<RecyclerView>(R.id.textBox)
+        recyclerView.layoutManager = LinearLayoutManager(this@messageBox,
+            RecyclerView.VERTICAL,true)
+        val adapter = MessageBoxAdapter(dummyData.getDummData())
+        recyclerView.adapter = adapter
     }
 }
