8c46ada6836716ef8f5ec801075c1adc355dca05
Achintya Sharma <sharmaachintya08@gmail.com>
2023-02-06 00:45:19 +0530

    runtime permission added

--- app/src/main/java/com/example/chatc/enter/signInActivity.kt
+++ app/src/main/java/com/example/chatc/enter/signInActivity.kt
@@ -2,6 +2,8 @@ package com.example.chatc.enter
 
 import android.content.ContentValues.TAG
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -10,6 +12,9 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.chatc.R
 import com.example.chatc.Validity.ifValid
 import com.example.chatc.data.firestoreInstance
@@ -26,9 +31,13 @@ class signInActivity : AppCompatActivity(),View.OnClickListener {
     private lateinit var email : EditText
     private lateinit var password : EditText
 
+    private val POST_NOTIFICATION : Int = 100
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        checkForPermissions(android.Manifest.permission.POST_NOTIFICATIONS,"notifications",POST_NOTIFICATION)
+
         signInButton = findViewById(R.id.buttonSignIn)
         createNewAccountText = findViewById(R.id.textCreateNewAccount)
         guestContinue = findViewById(R.id.guestContinue)
@@ -64,5 +73,46 @@ class signInActivity : AppCompatActivity(),View.OnClickListener {
         }else{
             Log.d("debug","no if else option selected")
         }
+    }
+    private fun checkForPermissions(permission : String,name : String,requestCode : Int){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            when{
+                ContextCompat.checkSelfPermission(applicationContext,permission) == PackageManager.PERMISSION_GRANTED -> {
+                    Toast.makeText(applicationContext,"$name permission granted",Toast.LENGTH_SHORT).show()
+                }
+                shouldShowRequestPermissionRationale(permission) -> showDialog(permission,name,requestCode)
+                else -> ActivityCompat.requestPermissions(this@signInActivity, arrayOf(permission),requestCode)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        fun innerCheck(name : String){
+            if(grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(applicationContext,"$name permission refused",Toast.LENGTH_SHORT).show()
+            }else{
+                Toast.makeText(applicationContext,"$name permission granted",Toast.LENGTH_SHORT).show()
+            }
+        }
+        when(requestCode){
+            POST_NOTIFICATION -> innerCheck("POST_NOTIFICATIONS")
+        }
+    }
+    private fun showDialog(permission : String,name : String,requestCode: Int){
+        val builder = AlertDialog.Builder(this@signInActivity)
+        builder.apply {
+            setMessage("Permission to access your $name is required to use this app")
+            setTitle("Permission required")
+            setPositiveButton("ACCEPT"){dialog,which ->
+                ActivityCompat.requestPermissions(this@signInActivity, arrayOf(permission),requestCode)
+            }
+        }
+        val dialog = builder.create()
+        dialog.show()
     }
 }
