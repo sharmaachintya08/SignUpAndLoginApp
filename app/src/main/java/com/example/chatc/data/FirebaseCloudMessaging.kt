e1b63948067f1fb03077ca907a30414465cadefb
Achintya Sharma <sharmaachintya08@gmail.com>
2023-02-07 00:15:13 +0530

    buggy commit , exception :- java.lang.runtimeException

--- app/src/main/java/com/example/chatc/data/FirebaseCloudMessaging.kt
+++ app/src/main/java/com/example/chatc/data/FirebaseCloudMessaging.kt
@@ -1,18 +1,16 @@
 package com.example.chatc.data
 
 import android.util.Log
-import com.example.chatc.R
-import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService
 
 object FirebaseCloudMessaging : FirebaseMessagingService() {
-    val getToken =  FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
-        if (!task.isSuccessful) {
-            Log.w("token", "Fetching FCM registration token failed", task.exception)
-            return@OnCompleteListener
+    val getToken = FirebaseMessaging.getInstance().token
+        .addOnCompleteListener{ task ->
+            if(!task.isSuccessful){
+                Log.d("token","fcm registeration token cannot be retrieved ${task.exception}")
+            }
+            val token = task.result
+            Log.i("token","$token")
         }
-        val token = task.result
-        Log.i("token","$token")
-    })
 }
