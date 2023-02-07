0fc0b91f2c604bd6ef19bb9833ad951f075a49f7
Achintya Sharma <sharmaachintya08@gmail.com>
2023-02-07 18:20:20 +0530

    removed permission methods

--- app/src/main/java/com/example/chatc/enter/signInActivity.kt
+++ app/src/main/java/com/example/chatc/enter/signInActivity.kt
@@ -33,11 +33,10 @@ class signInActivity : AppCompatActivity(),View.OnClickListener {
     private lateinit var password : EditText
 
     private val POST_NOTIFICATION : Int = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
-
-        checkForPermissions(android.Manifest.permission.POST_NOTIFICATIONS,"notifications",POST_NOTIFICATION)
 
         signInButton = findViewById(R.id.buttonSignIn)
         createNewAccountText = findViewById(R.id.textCreateNewAccount)
@@ -74,46 +73,5 @@ class signInActivity : AppCompatActivity(),View.OnClickListener {
         }else{
             Log.d("debug","no if else option selected")
         }
-    }
-    private fun checkForPermissions(permission : String,name : String,requestCode : Int){
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-            when{
-                ContextCompat.checkSelfPermission(applicationContext,permission) == PackageManager.PERMISSION_GRANTED -> {
-                    Toast.makeText(applicationContext,"$name permission granted",Toast.LENGTH_SHORT).show()
-                }
-                shouldShowRequestPermissionRationale(permission) -> showDialog(permission,name,requestCode)
-                else -> ActivityCompat.requestPermissions(this@signInActivity, arrayOf(permission),requestCode)
-            }
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        fun innerCheck(name : String){
-            if(grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED){
-                Toast.makeText(applicationContext,"$name permission refused",Toast.LENGTH_SHORT).show()
-            }else{
-                Toast.makeText(applicationContext,"$name permission granted",Toast.LENGTH_SHORT).show()
-            }
-        }
-        when(requestCode){
-            POST_NOTIFICATION -> innerCheck("POST_NOTIFICATIONS")
-        }
-    }
-    private fun showDialog(permission : String,name : String,requestCode: Int){
-        val builder = AlertDialog.Builder(this@signInActivity)
-        builder.apply {
-            setMessage("Permission to access your $name is required to use this app")
-            setTitle("Permission required")
-            setPositiveButton("ACCEPT"){dialog,which ->
-                ActivityCompat.requestPermissions(this@signInActivity, arrayOf(permission),requestCode)
-            }
-        }
-        val dialog = builder.create()
-        dialog.show()
     }
 }
