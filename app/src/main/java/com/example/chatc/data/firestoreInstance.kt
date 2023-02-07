10fb1a8dd46cbfa87862454fbcd78f1456895286
Achintya Sharma <sharmaachintya08@gmail.com>
2023-02-07 15:13:37 +0530

    added a calling statement for firebasecloudmessaging

--- app/src/main/java/com/example/chatc/data/firestoreInstance.kt
+++ app/src/main/java/com/example/chatc/data/firestoreInstance.kt
@@ -72,6 +72,7 @@ class firestoreInstance (
                     if(dataList.contains(email)&&dataList.contains(password)){
                         var intent = Intent(con,messageBox::class.java)
                         con.startActivity(intent)
+                        FirebaseCloudMessaging.getToken
                     }else{
                         Toast.makeText(con,"id not present please signup",Toast.LENGTH_SHORT)
                             .show()
