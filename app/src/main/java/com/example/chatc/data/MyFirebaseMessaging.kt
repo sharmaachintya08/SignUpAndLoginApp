1d2c298730aa7a5ad915df2d49d9494aadf8756f
Achintya Sharma <sharmaachintya08@gmail.com>
2023-02-09 02:41:15 +0530

    removed commented methods

--- app/src/main/java/com/example/chatc/data/MyFirebaseMessaging.kt
+++ app/src/main/java/com/example/chatc/data/MyFirebaseMessaging.kt
@@ -10,21 +10,6 @@ import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
 object MyFirebaseMessaging : FirebaseMessagingService() {
-    /*private val channelId = "chatc"
-    override fun onMessageReceived(message: RemoteMessage) {
-        super.onMessageReceived(message)
-        getFirebaseMessaging(message.notification?.title.toString(),message.notification?.body.toString())
-    }
-    fun getFirebaseMessaging(title : String,body : String){
-        Log.i("token",title)
-        val builder = NotificationCompat.Builder(applicationContext,channelId)
-            .setSmallIcon(R.drawable.ic_baseline_circle_notifications_24)
-            .setContentTitle(title)
-            .setContentText(body)
-            .setAutoCancel(true)
-        val manager = NotificationManagerCompat.from(applicationContext)
-        manager.notify(101,builder.build())
-    }*/
     val getToken = FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
         if(!task.isSuccessful){
             Log.i("token",task.exception.toString())
