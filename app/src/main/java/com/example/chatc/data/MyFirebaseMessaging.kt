0e8a96b6434805cdfcfb1c06e30e4b8caea0869a
Achintya Sharma <sharmaachintya08@gmail.com>
2023-02-09 02:39:29 +0530

    checkpoint :- getting notifications when the app is running on background

--- app/src/main/java/com/example/chatc/data/MyFirebaseMessaging.kt
+++ app/src/main/java/com/example/chatc/data/MyFirebaseMessaging.kt
@@ -1,18 +1,22 @@
 package com.example.chatc.data
 
+import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.example.chatc.R
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
-class MyFirebaseMessaging : FirebaseMessagingService() {
-    private val channelId = "chatc"
+object MyFirebaseMessaging : FirebaseMessagingService() {
+    /*private val channelId = "chatc"
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
         getFirebaseMessaging(message.notification?.title.toString(),message.notification?.body.toString())
     }
     fun getFirebaseMessaging(title : String,body : String){
+        Log.i("token",title)
         val builder = NotificationCompat.Builder(applicationContext,channelId)
             .setSmallIcon(R.drawable.ic_baseline_circle_notifications_24)
             .setContentTitle(title)
@@ -20,5 +24,12 @@ class MyFirebaseMessaging : FirebaseMessagingService() {
             .setAutoCancel(true)
         val manager = NotificationManagerCompat.from(applicationContext)
         manager.notify(101,builder.build())
-    }
+    }*/
+    val getToken = FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
+        if(!task.isSuccessful){
+            Log.i("token",task.exception.toString())
+            return@OnCompleteListener
+        }
+        Log.i("token",task.result.toString())
+    })
 }
