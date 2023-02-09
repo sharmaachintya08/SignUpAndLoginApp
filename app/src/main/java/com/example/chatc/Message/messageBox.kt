268a4c47bfeb6c3b3b3efaf94fda0d112abda5f3
Achintya Sharma <sharmaachintya08@gmail.com>
2023-02-09 18:42:49 +0530

    message is being recieved from the editText

--- app/src/main/java/com/example/chatc/Message/messageBox.kt
+++ app/src/main/java/com/example/chatc/Message/messageBox.kt
@@ -2,16 +2,23 @@ package com.example.chatc.Message
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.chatc.R
 import com.example.chatc.data.dummyData
 
 open class messageBox : AppCompatActivity() {
+    private lateinit var messageEditText : EditText
+    private lateinit var sendButton : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_message_box)
         recyclerViewReference()
+        noteText()
     }
     fun recyclerViewReference(){
         val recyclerView = findViewById<RecyclerView>(R.id.textBox)
@@ -19,5 +26,16 @@ open class messageBox : AppCompatActivity() {
             RecyclerView.VERTICAL,true)
         val adapter = MessageBoxAdapter(dummyData.getDummData())
         recyclerView.adapter = adapter
+    }
+    fun noteText(){
+        getReference()
+        sendButton.setOnClickListener(View.OnClickListener { view ->
+            val text = messageEditText.text.toString()
+            Log.d("text",text)
+        })
+    }
+    fun getReference(){
+        messageEditText = findViewById(R.id.sendMessage)
+        sendButton = findViewById(R.id.sendMessageButton)
     }
 }
