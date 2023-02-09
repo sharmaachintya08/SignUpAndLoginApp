46bec219d37e8a12fea35ecdb1eb96bb8d348682
Achintya Sharma <sharmaachintya08@gmail.com>
2023-02-09 18:42:30 +0530

    added onMessageRecieve method

--- app/src/main/java/com/example/chatc/data/MyFirebaseMessaging.kt
+++ app/src/main/java/com/example/chatc/data/MyFirebaseMessaging.kt
@@ -8,13 +8,25 @@ import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.google.firebase.messaging.ktx.remoteMessage
 
 object MyFirebaseMessaging : FirebaseMessagingService() {
+    private val TAG = "firebaseservice"
     val getToken = FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
         if(!task.isSuccessful){
-            Log.i("token",task.exception.toString())
+            Log.i(TAG,task.exception.toString())
             return@OnCompleteListener
         }
         Log.i("token",task.result.toString())
     })
+
+    override fun onMessageReceived(message: RemoteMessage) {
+        Log.d(TAG,"From : ${message.from}")
+        if(message.data.isNotEmpty()){
+            Log.d(TAG,"Message data payload : ${message.data}")
+        }
+        message.notification?.let {
+            Log.d(TAG, "Message Notification Body: ${it.body}")
+        }
+    }
 }
