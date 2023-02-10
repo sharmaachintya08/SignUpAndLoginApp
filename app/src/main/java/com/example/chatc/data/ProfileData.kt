e141ec183fc6ac0197f1e2f1df8bd0a54b33b0bd
Achintya Sharma <sharmaachintya08@gmail.com>
2023-02-10 16:28:45 +0530

    created getters and setter for object data

--- app/src/main/java/com/example/chatc/data/ProfileData.kt
+++ app/src/main/java/com/example/chatc/data/ProfileData.kt
@@ -3,40 +3,20 @@ package com.example.chatc.data
 import com.google.firebase.storage.StorageReference
 
 data class ProfileData(
-    val imageRef : StorageReference?,
     val name : String,
     val message : String
 )
 
-object dummyData{
-    fun getDummData(): ArrayList<ProfileData>{
-        val item1 = ProfileData(null,"a","message a")
-        val item2 = ProfileData(null,"b","message b")
-        val item3 = ProfileData(null,"c","message c")
-        val item4 = ProfileData(null,"d","message d")
-        val item5 = ProfileData(null,"e","message e")
-        val item6 = ProfileData(null,"f","message f")
-        val item7 = ProfileData(null,"g","message g")
-        val item8 = ProfileData(null,"h","message h")
-        val item9 = ProfileData(null,"i","message i")
-        val item10 = ProfileData(null,"j","message j")
-        val item11 = ProfileData(null,"k","message k")
-        val item12 = ProfileData(null,"l","message l")
-
+object Data{
+    private var ftext : String = "lets chat everyone"
+    private var femail : String = "bot"
+    fun setData(text: String,email : String){
+        ftext = text
+        femail = email
+    }
+    fun getData() : ArrayList<ProfileData>{
         val dataList : ArrayList<ProfileData> = ArrayList()
-        dataList.add(item1)
-        dataList.add(item2)
-        dataList.add(item3)
-        dataList.add(item4)
-        dataList.add(item5)
-        dataList.add(item6)
-        dataList.add(item7)
-        dataList.add(item8)
-        dataList.add(item9)
-        dataList.add(item10)
-        dataList.add(item11)
-        dataList.add(item12)
-
+        dataList.add(ProfileData(ftext,femail))
         return dataList
     }
 }
